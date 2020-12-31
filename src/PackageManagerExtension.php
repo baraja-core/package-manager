66c09e6200121d091ae40c7579281c31825b2fca
Jan Barášek <janbarasek@gmail.com>
2020-12-31 18:27:25 +0100

    Remove Console and move to package baraja-core/nette-symfony-console.

--- src/PackageManagerExtension.php
+++ src/PackageManagerExtension.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace Baraja\PackageManager;
 
 
-use Baraja\PackageManager\Console\Console;
 use Nette\DI\CompilerExtension;
-use Nette\PhpGenerator\ClassType;
-use Symfony\Component\Console\Application;
 
 final class PackageManagerExtension extends CompilerExtension
 {
@@ -19,20 +16,5 @@ final class PackageManagerExtension extends CompilerExtension
 		$builder->addDefinition($this->prefix('packageRegistrator'))
 			->setFactory(PackageRegistrator::class)
 			->setAutowired(PackageRegistrator::class);
-	}
-
-
-	public function afterCompile(ClassType $class): void
-	{
-		if (PHP_SAPI === 'cli') {
-			$class->getMethod('initialize')->addBody(
-				'// Package manager.' . "\n"
-				. '(function () {' . "\n"
-				. "\t" . 'if (isset($_SERVER[\'NETTE_TESTER_RUNNER\']) === true) { return; }' . "\n"
-				. "\t" . 'new ' . Console::class . '($this->getByType(?), $this->getByType(?));' . "\n"
-				. '})();' . "\n",
-				[Application::class, \Nette\Application\Application::class]
-			);
-		}
 	}
 }
