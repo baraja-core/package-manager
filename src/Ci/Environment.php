48001b9fa5d4732fb56a0981825ba059638f226c
Jan Barášek <janbarasek@gmail.com>
2021-02-17 20:48:57 +0100

    Environment::get(): PHP 8 return type compatibility.

--- src/Ci/Environment.php
+++ src/Ci/Environment.php
@@ -13,6 +13,7 @@ final class Environment
 	/** Environment variable value or false if the variable does not exist. */
 	public function get(string $name): string|false
 	{
+		/** @var string|mixed[]|false $env */
 		$env = getenv($name);
 
 		return \is_array($env) === true ? false : $env;
